aca495a7e7cf97a8d91f946abf9ca48012514193
Tristan <cantristan3@gmail.com>
2022-11-14 23:32:20 +0700

    commit

--- resources/views/layout/temp.blade.php
+++ resources/views/layout/temp.blade.php
@@ -8,6 +8,19 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <title>@yield('title')</title>
 </head>
+    
+<style>
+    body {
+      background-image: url('https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80');
+    }
+</style>
+<nav>
+    @include('layout.nav')
+</nav>
+
+<header>
+       {{-- @include('layout.header') --}}
+</header>
 <body>
     @yield('content')
 </body>
--- resources/views/zmcs.blade.php
+++ resources/views/zmcs.blade.php
@@ -1,21 +1,19 @@
 @extends('layout.temp')
-
 @section('content')
-    @include('layout.nav')
     <div class="d-flex justify-content-between">
     @for($i=0;$i<count($cloth);$i++)
-            <div class="card" style="width: 20rem;">
-            <img src={{ $photo[$i] }} class="card-img-top mx-auto d-block" style="margin:20px; width:200px; height:200px;" alt="Card image cap">
+            <div class="card" style="width: 20rem;background-color: grey">
+            <img src={{ $path[$i] }} class="card-img-top mx-auto d-block" style="margin:20px; width:200px; height:200px;" alt="Card image cap">
             <div class="card-body" style="d-flex flex-column">
                 <h5 class="card-title justify-content-center" style="text-align:center">{{ $cloth[$i] }}</h5>
                 <p class="card-text" style="text-align:center">{{ $desc[$i] }}</p>
                 <a href="#" class="btn btn-primary d-flex justify-content-center" style="margin: 10px; text-align:center; align-items:center">Buy Now</a>
-                <h5 class="card-text" style="text-align:center"> Stock : {{ $stok[$i] }}</h5>
-                @if($stok[$i] > 0)
+                <h5 class="card-text" style="text-align:center"> Stock : {{ $stock[$i] }}</h5>
+                @if($stock[$i] > 0)
                     <div class="alert alert-success d-inline-bock" style="text-align:center">
                         Stock Available
                     </div>
-                @elseif($stok[$i] == 0)
+                @elseif($stock[$i] == 0)
                     <div class="alert alert-danger d-inline-bock" style="text-align:center">
                         Stock Not Available
                     </div>
